c9eedacf49583039eb025903770a86496b087c91
IsaqSanfer <isaelsferreira@outlook.com>
2023-11-29 21:42:28 -0300

    Tratamento para a solution

---
+++ UncafezinWeb/Entities/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UncafezinWeb.Entities
+{
+    public class Account
+    {
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LogOnModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool RemembreMe { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
